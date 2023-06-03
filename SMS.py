775c6be96e1a50024860cc1c37ca6f87f55a62ac
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-03 20:16:15 +0100

    Update SMS.py

--- SMS.py
+++ SMS.py
@@ -5,7 +5,7 @@ from twilio.rest import Client
 st.title("SMS Sender")
 
 account_sid = "ACcb2dd042df686a03ed1507464a43cbc3"
-auth_token = '3408a5e648b334ec1acf8c160faa569e'
+auth_token = "62db30d3466492cf4af4094a8707f2da"
 from_number = '+12053016406'
 to_number = st.text_input("Recipient Phone Number")
 message_body = st.text_area("Message Body")
